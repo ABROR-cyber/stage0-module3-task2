4adccde9edc64f8405ac8a6dba6d7f773e298fd2
abror <abror01042001@gmail.com>
2022-07-27 00:35:02 +0500

    fixx

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,10 +1,7 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
-    private static final int ADULTAGE = 18;
 
-    private int age;
-    private int phoneNumber;
 
     void callToFriend() {
         //some code
